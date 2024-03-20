16263a093f43f33c128b49dcfbb519188e76927d
bati finkelshtein <b@gmail.com>
2024-03-20 18:35:22 +0200

    aaa'

---
+++ Controllers/Login.cs
@@ -0,0 +1,16 @@
+// using System.Collections.Generic;
+// using System.Linq;
+// using Microsoft.AspNetCore.Mvc;
+// using todoList.Models;
+// using todoList.Interfaces;
+// using Microsoft.AspNetCore.Authorization;
+
+// namespace todoList.Controllers;
+
+// [ApiController,Aut]
+// [Route("todo")]
+// public class loginController : ControllerBase
+// {
+ 
+     
+// }
---
+++ Models/JWTTokenResponse.cs
@@ -0,0 +1,6 @@
+namespace todoList.Models;
+
+    public class JWTTokenResponse
+    {
+        public string? Token { get; set; }
+    }
