253e2a244105080fbb1ee786ae405257fe94e4da
הנדסת תוכנה <huser@sem.local>
2024-04-08 13:51:13 +0300

    add task working

--- wwwroot/js/s.js
+++ wwwroot/js/s.js
@@ -2,13 +2,13 @@ const uri = '/todoTask';
 const token = localStorage.getItem("token")
 const auth = `Bearer ${token} `
 
-let myuser=document.getElementById('user');
+let myuser = document.getElementById('user');
 
-const drawUser=(user)=>{
-    const div=document.createElement('div');
-    const h2=document.createElement('h2');
-    h2.innerHTML=`hello to ${user.userName} 😀😊😉`
-    h2.style.color='black'
+const drawUser = (user) => {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `hello to ${user.userName} 😀😊😉`
+    h2.style.color = 'black'
     div.appendChild(h2);
     myuser.appendChild(div);
 
@@ -23,60 +23,121 @@ fetch('User/GetUser', {
     })
     .then(response => response.json())
     .then((data) => {
-       
+
         drawUser(data);
 
     })
     .catch((error) => {
-      
+
         alert("please login again👍")
-});
-const deleteTask=async (task)=>
-{
+    });
+const deleteTask = async(task) => {
     await fetch(`todo/DeleteTask/${task.id}`, {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        
-    })
-    .then(response => response.json())
-    .then((res) => {if(res==true){alert(`the delete is success 👌👌👌 `)}})
-.catch(error => console.error('Unable to enter to site please speak with the manager', error));
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+
+        })
+        .then(response => response.json())
+        .then((res) => { if (res == true) { alert(`the delete is success 👌👌👌 `) } })
+        .catch(error => console.error('Unable to enter to site please speak with the manager', error));
 }
-const taskPut= async(task)=>{
+const taskPut = async(task) => {
     await fetch(`todo/UpdateTask/${task.id}`, {
-        method: 'PUT',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(task)
-    })
-    .then(response => response.json())
-    .then((res) => {if(res==true){alert(`the edit is success 👌👌👌 `)}})
-.catch(error => console.error('Unable to enter to site please speak with the manager', error));
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(task)
+        })
+        .then(response => response.json())
+        .then((res) => { if (res == true) { alert(`the edit is success 👌👌👌 `) } })
+        .catch(error => console.error('Unable to enter to site please speak with the manager', error));
 };
+const SendNewTask = (Taskname, isdone) => {
+    const task = {
+        "id": 0,
+        "name": Taskname,
+        "isDone": isdone
+    }
+    fetch('todo/AddTask', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(task)
+        })
+        .then(response => response.json())
+        .then((res) => { if (res > 0) { task.id = res, drawTask(task); } })
+        .then((res) => { if (res > 0) { alert(`the add is success 👌👌👌 `) } })
+
+    .catch(error => console.error('Unable to enter to site please speak with the manager', error));
+
+}
 const tasks = document.getElementById('tasks');
-const drawTable=()=>{
-    const table=document.createElement('table');
-    table.id='tasksList';
-    tr=document.createElement('tr');
-    const thEdit=document.createElement('th');
-    thEdit.innerHTML='Edit'
-    const thId=document.createElement('th');
-    thId.innerHTML='Id'
-    const thName=document.createElement('th');
-    thName.innerHTML='Name'
-    const thIsDone=document.createElement('th');
-    thIsDone.innerHTML='IsDone'
-    const thDelete=document.createElement('th');
-    thDelete.innerHTML='Delete'
-    const tbody=document.createElement('tbody');
-    tbody.id='tasksTable';
+const drawTable = () => {
+    const table = document.createElement('table');
+    table.id = 'tasksList';
+    const trNew = document.createElement('tr');
+    const thNew = document.createElement('th');
+    const button = document.createElement('button');
+    button.innerHTML = 'to add new task🪶';
+    thNew.appendChild(button);
+    trNew.appendChild(thNew);
+    table.appendChild(trNew)
+    button.onclick = () => {
+        const name = document.createElement('th')
+        const nameInput = document.createElement('input')
+        nameInput.type = Text;
+        const labelth = document.createElement('th')
+        nameInput.placeholder = 'name of the task'
+        const label = document.createElement('label');
+        label.innerHTML = 'is done?'
+        labelth.appendChild(label)
+        const isDone = document.createElement('th')
+        const isDoneInput = document.createElement('input')
+        isDoneInput.type = 'checkbox';
+        const save = document.createElement('th');
+        const buttonSave = document.createElement('button');
+        buttonSave.innerHTML = 'save'
+        buttonSave.onclick = () => {
+            SendNewTask(nameInput.value, isDoneInput.checked);
+        }
+        save.appendChild(buttonSave);
+
+        name.appendChild(nameInput);
+        isDone.appendChild(isDoneInput);
+        trNew.appendChild(name);
+        trNew.appendChild(labelth)
+        trNew.appendChild(isDone);
+        trNew.appendChild(save)
+
+
+
+
+    }
+
+
+    tr = document.createElement('tr');
+    const thEdit = document.createElement('th');
+    thEdit.innerHTML = 'Edit'
+    const thId = document.createElement('th');
+    thId.innerHTML = 'Id'
+    const thName = document.createElement('th');
+    thName.innerHTML = 'Name'
+    const thIsDone = document.createElement('th');
+    thIsDone.innerHTML = 'IsDone'
+    const thDelete = document.createElement('th');
+    thDelete.innerHTML = 'Delete'
+    const tbody = document.createElement('tbody');
+    tbody.id = 'tasksTable';
     tr.appendChild(thEdit);
     tr.appendChild(thId);
     tr.appendChild(thName);
@@ -107,7 +168,7 @@ const drawTask = (task) => {
     const isDoneInput = document.createElement('input')
     isDoneInput.type = 'checkbox';
 
-    isDoneInput.checked=task.isDone;
+    isDoneInput.checked = task.isDone;
 
     const deletTask = document.createElement('th')
     const deleteButton = document.createElement('button')
@@ -121,15 +182,14 @@ const drawTask = (task) => {
             editButton.innerHTML = 'ok'
             nameInput.disabled = false;
             isDoneInput.disabled = false;
-         
-        }
-        else {
+
+        } else {
             editButton.innerHTML = 'edit'
             nameInput.disabled = true;
             isDoneInput.disabled = true;
-            task.id=task.id;
-            task.name=nameInput.value;
-            task.isDone=isDoneInput.checked;
+            task.id = task.id;
+            task.name = nameInput.value;
+            task.isDone = isDoneInput.checked;
             taskPut(task);
         }
     }
@@ -152,9 +212,9 @@ const drawTask = (task) => {
 
     tasksTable.appendChild(tr)
 }
-const myTasks=document.getElementById('myTasks')
+const myTasks = document.getElementById('myTasks')
 const getMyTasks = () => {
-    myTasks.style='display:none';
+    myTasks.style = 'display:none';
     fetch('todo/GetMyTasks', {
             method: 'Get',
             headers: {
@@ -164,11 +224,12 @@ const getMyTasks = () => {
             },
         })
         .then(response => response.json())
-        
-        .then((data) => {drawTable(),
-            data.forEach(x => {
-                drawTask(x);
-            });  
+
+    .then((data) => {
+            drawTable(),
+                data.forEach(x => {
+                    drawTask(x);
+                });
 
         })
         .catch((error) => {
@@ -177,8 +238,3 @@ const getMyTasks = () => {
             location.href = 'index.html'
         });
 }
-
-
-
-
-   
